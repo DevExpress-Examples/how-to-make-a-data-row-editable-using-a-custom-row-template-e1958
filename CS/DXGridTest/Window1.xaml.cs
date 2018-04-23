684051be1d594565096b04ab93d219f514e1ec77
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGridTest/Window1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Utils.Themes;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Core;
+using System.Collections;
+using System.Windows.Threading;
+
+namespace DXGridTest {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+
+            List<TestData> list = new List<TestData>();
+            for(int i = 0; i < 100; i++)
+                list.Add(new TestData() { Text = "Row" + i, Number = i, Group = i % 3 });
+            grid.ItemsSource = list;
+        }
+    }
+
+    public class MyTableView : TableView {
+        protected override void OnKeyUp(KeyEventArgs e) {
+        }
+        protected override void OnKeyDown(KeyEventArgs e) {
+            if(e.Key == Key.Tab)
+                return;
+            base.OnKeyDown(e);
+        }
+    }
+
+    public class TestData {
+        public string Text { get; set; }
+        public int Number { get; set; }
+        public int Group { get; set; }
+    }
+}
---
+++ VB/DXGridTest/Window1.xaml.vb
@@ -0,0 +1,79 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Utils.Themes
+Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Core
+Imports System.Collections
+Imports System.Windows.Threading
+
+Namespace DXGridTest
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+
+			Dim list As New List(Of TestData)()
+			For i As Integer = 0 To 99
+				list.Add(New TestData() With {.Text = "Row" & i, .Number = i, .Group = i Mod 3})
+			Next i
+			grid.ItemsSource = list
+		End Sub
+	End Class
+
+	Public Class MyTableView
+		Inherits TableView
+		Protected Overrides Sub OnKeyUp(ByVal e As KeyEventArgs)
+		End Sub
+		Protected Overrides Sub OnKeyDown(ByVal e As KeyEventArgs)
+			If e.Key = Key.Tab Then
+				Return
+			End If
+			MyBase.OnKeyDown(e)
+		End Sub
+	End Class
+
+	Public Class TestData
+		Private privateText As String
+		Public Property Text() As String
+			Get
+				Return privateText
+			End Get
+			Set(ByVal value As String)
+				privateText = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+		Private privateGroup As Integer
+		Public Property Group() As Integer
+			Get
+				Return privateGroup
+			End Get
+			Set(ByVal value As Integer)
+				privateGroup = value
+			End Set
+		End Property
+	End Class
+End Namespace
